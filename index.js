447b9e998bdc39b717d3738641689c08d8f9eff7
Zee <zee@simplyzee.dev>
2021-08-29 20:12:11 +0100

    Adding module.exports handler for lambda

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-const AWS = require('aws-sdk'),
-  fs = require('fs'),
-  parse = require('csv-parse/lib/sync'),
-  csv=require('csvtojson');
+"use strict";
 
+const AWS = require("aws-sdk"),
+  csv = require("csvtojson");
 
-AWS.config.update({
-  region: "eu-west-2"
-});
+module.exports.handler = (event) => {
+  console.log("received", event);
 
-const s3 = new AWS.S3();
-const client = new AWS.DynamoDB.DocumentClient();
+  AWS.config.update({
+    region: "eu-west-2",
+  });
 
-const params = {
-  Bucket: 'csv-cities-example-bucket',
-  Key: 'cities.csv',
-};
+  const s3 = new AWS.S3();
+  const client = new AWS.DynamoDB.DocumentClient();
 
-async function csvToDynamoDB() {
+  const params = {
+    Bucket: "csv-cities-example-bucket",
+    Key: "cities.csv",
+  };
+
   const stream = s3.getObject(params).createReadStream();
 
-  csv().fromStream(stream).on('data', (row) => {
-    let content = JSON.parse(row);
+  csv()
+    .fromStream(stream)
+    .on("data", (row) => {
+      let content = JSON.parse(row);
 
-    console.log(JSON.stringify(content))
+      console.log(JSON.stringify(content));
 
-    let pushParams = {
-      TableName: "PremierLeague",
-      Item: {
-        "Date": Date().now(),
-        "City": content.City,
-        "State": content.State,
-      }
-    };
+      let pushParams = {
+        TableName: "PremierLeague",
+        Item: {
+          Date: Date().now(),
+          City: content.City,
+          State: content.State,
+        },
+      };
 
-    client.put(pushParams, (err, res) => {
-      
-      if(err)
-        console.log(err)
-      else {
-        console.log("Added item:", JSON.stringify(pushParams.Item, null, 2));
-      }
-    })
-  })
+      client.put(pushParams, (err, res) => {
+        if (err) console.log(err);
+        else {
+          console.log("Added item:", JSON.stringify(pushParams.Item, null, 2));
+        }
+      });
+    });
 }
-
-csvToDynamoDB();
